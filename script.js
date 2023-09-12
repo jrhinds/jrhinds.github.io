769e4e537eaf50401a146f1d9768d838c8680032
Jonathan R. Hinds <98826425+jrhinds@users.noreply.github.com>
2023-09-12 15:21:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,20 +4,28 @@ document.addEventListener("DOMContentLoaded", function() {
 
     document.getElementById('generate-curl').addEventListener('click', function() {
         let selectedActions = Array.from(form['repo-actions']).filter(checkbox => checkbox.checked).map(checkbox => checkbox.value);
-        
-        // Generate cURL commands based on selected actions
+
         let curlCommands = selectedActions.map(action => {
             switch(action) {
                 case "GET /repos/:owner/:repo":
-                    return `curl -H "Authorization: token YOUR_TOKEN" "https://api.github.com/repos/:owner/:repo"`;
+                    const owner = document.getElementById('owner').value;
+                    const repo = document.getElementById('repo').value;
+                    return `curl -H "Authorization: token YOUR_TOKEN" "https://api.github.com/repos/${owner}/${repo}"`;
+                
                 case "GET /user/repos":
                     return `curl -H "Authorization: token YOUR_TOKEN" "https://api.github.com/user/repos"`;
+
+                case "GET /repos/:owner/:repo/branches":
+                    const ownerBranches = document.getElementById('owner-branches').value;
+                    const repoBranches = document.getElementById('repo-branches').value;
+                    return `curl -H "Authorization: token YOUR_TOKEN" "https://api.github.com/repos/${ownerBranches}/${repoBranches}/branches"`;
+                
                 // Add more cases based on the options
                 default:
                     return '';
             }
         });
-        
+
         curlCommandArea.value = curlCommands.join('\n');
     });
 });
